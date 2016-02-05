ab8067e79072842bca9f9045ede993353e16baee
Daniel Kronovet <kronovet@gmail.com>
2016-02-04 21:18:56 -0500

    implemented canonical is_sorted

--- algorithms/is_sorted.py
+++ algorithms/is_sorted.py
@@ -1,35 +1,37 @@
+"""Runs in-order iterative depth-first search through a binary tree,
+    to check if the nodes are correctly ordered.
+
+Objectives:
+    O(m + n) running time
+
+Args:
+    bt: a binary tree, with nodes encoded as integers.
+
+Returns:
+    True if the elements of the tree are sorted, False otherwise.
+
+>>> bt = binary_tree(6)
+>>> bt.set_left(2)
+>>> bt.left.set_left(1)
+>>> bt.left.set_right(4)
+>>> bt.left.right.set_left(3)
+>>> bt.left.right.set_right(5)
+>>> bt.set_right(8)
+>>> bt.right.set_left(7)
+>>> bt.right.set_right(9)
+>>> is_sorted(bt)
+True
+
+>>> bt.right.set_left(5)
+>>> is_sorted(bt)
+False
+"""
+
 from datastructures.stack import stack
 from datastructures.binary_tree import binary_tree
 
-def is_sorted(bt):
-    """Runs in-order iterative depth-first search through a binary tree,
-        to check if the nodes are correctly ordered.
-
-    Objectives:
-        O(m + n) running time
-
-    Args:
-        bt: a binary tree, with nodes encoded as integers.
-
-    Returns:
-        True if the elements of the tree are sorted, False otherwise.
-
-    >>> bt = binary_tree(6)
-    >>> bt.set_left(2)
-    >>> bt.left.set_left(1)
-    >>> bt.left.set_right(4)
-    >>> bt.left.right.set_left(3)
-    >>> bt.left.right.set_right(5)
-    >>> bt.set_right(8)
-    >>> bt.right.set_left(7)
-    >>> bt.right.set_right(9)
-    >>> is_sorted(bt)
-    True
-
-    >>> bt.right.left.set_left(5)
-    >>> is_sorted(bt)
-    False
-    """
+# My Implementation
+def my_is_sorted(bt):
     s = stack()
     explored = set()
     curr_max = 0
@@ -61,16 +63,28 @@ def is_sorted(bt):
 
     return True
 
-bt = binary_tree(6)
-bt.set_left(2)
-bt.left.set_left(1)
-bt.left.set_right(4)
-bt.left.right.set_left(3)
-bt.left.right.set_right(5)
-bt.set_right(8)
-bt.right.set_left(7)
-bt.right.set_right(9)
+### Canonical Implementation
+def canonical_is_sorted(node):
+    s = stack()
+    curr_max = 0
 
+    while not s.is_empty or node is not None:
+        if node is not None:
+            s.push(node)
+            node = node.left
+        else:
+            node = s.pop()
+
+            if node.value < curr_max:
+                return False
+            else:
+                curr_max = node.value
+
+            node = node.right
+    return True
+
+is_sorted = my_is_sorted
+# is_sorted = canonical_is_sorted
 
 if __name__ == "__main__":
     import doctest
